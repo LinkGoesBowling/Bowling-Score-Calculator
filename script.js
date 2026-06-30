0ad6143f5981689469d4158834642a40b41a1b6d
LinkKelly <54011287+LinkGoesBowling@users.noreply.github.com>
2026-06-30 10:42:36 -0600

    Update script.js
    Was missing a quote that broke the entire script

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function addPins(count){
 		shot2Count = count;
 		let changeShot2 = document.getElementById("shot2");
 		changeShot2.textContent = shot2Count;
-		let changeFrame1 = document.getElementById("frame1);
+		let changeFrame1 = document.getElementById("frame1");
 		changeFrame1.textContent = score;
 		changeFrame1.style.color = black;
 	}
